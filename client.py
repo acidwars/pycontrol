c0ed38f184e3c063e3468d59cbbe6a6eb2ac04b9
Henrik <ginmaster@mailbox.org>
2017-03-10 15:44:36 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.6
 import socket
 import sys
+import argparser
 
 def Main():
     try:
--- server.py
+++ server.py
@@ -3,40 +3,53 @@ import socket
 import subprocess
 import sys
 import platform
+import argparse
 
 def Main():
-    try:
-        host = '172.30.96.124'
-        port = int(sys.argv[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbosity", help="increase output verbosity")
+    parser.add_argument("--ip", help="listening ip")
+    parser.add_argument("--port", help="listening port")
+    args = parser.parse_args()
+    if args.verbosity:
+        print("verbosity turned on")
+    while True:
+        try:
+            host = args.ip
+            port = int(args.port)
+            mySocket = socket.socket()
+            mySocket.bind((host,port))
+            mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     
-        mySocket = socket.socket()
-        mySocket.bind((host,port))
-        mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    
-        mySocket.listen(1)
-        conn, addr = mySocket.accept()
-        print ("Connection from: " + str(addr))
-        while True:
-            data = conn.recv(1024).decode()
-            if not data:
+            mySocket.listen(1)
+            conn, addr = mySocket.accept()
+            print ("Connection from: " + str(addr))
+            while True:
+                data = conn.recv(1024).decode()
+                if not data:
                     break
-            print ("from connected  user: " + str(data))
-            test = commands(data)
-            if test == None:
-                test = "none"
-            conn.send(test.encode())
+                print ("from connected  user: " + str(data))
+                test = commands(data)
+                if test == None:
+                    test = "none"
+                conn.send(test.encode())
             
-        conn.close()
+            conn.close()
 
-    except KeyboardInterrupt:
-        print("closing...")
-        conn.close()
-    except OSError as e:
-        print("Port is already in use :(")
-        print(e)
-    except IndexError:
-        print("Please specify a port!")
-        print(sys.argv[0] + " PORT")
+        except KeyboardInterrupt:
+            print("closing...")
+            if conn:
+                conn.close()
+                sys.exit("closing")
+            sys.exit("closing")
+        except OSError as e:
+            print("Port is already in use :(")
+            print(e)
+            sys.exit("closing")
+        except IndexError:
+            print("Please specify a port!")
+            print(sys.argv[0] + " PORT")
+            sys.exit("closing")
 
 
 def commands(cmd):
